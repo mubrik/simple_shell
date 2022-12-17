3d4621c587ae126d1acd8e94e99b63257165b022
mubrik <mubarakg4u@gmail.com>
2022-12-17 15:37:06 +0100

    changed exit to free shell_data

--- bin.c
+++ bin.c
@@ -11,13 +11,11 @@ int Bin_exit(__attribute__((__unused__)) shell_data_t *shell_d,
 {
 	int ex_code = 0;
 
-	if (cmd->argc <= 1)
-		exit(EXIT_SUCCESS);
-	if (!cmd->argv[1])
-		exit(EXIT_SUCCESS);
+	if (cmd->argc <= 1 || !cmd->argv[1])
+		free_shell_data(shell_d), exit(EXIT_SUCCESS);
 	/* convert arg to int, unsafe */
 	ex_code = _atoi(cmd->argv[1]);
-	exit(ex_code);
+	free_shell_data(shell_d), exit(ex_code);
 }
 
 /**

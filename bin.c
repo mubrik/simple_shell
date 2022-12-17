e282961eea76d40c175249bb22fcb62e5ba12d75
mubrik <mubarakg4u@gmail.com>
2022-12-17 15:37:51 +0100

    tweka

--- bin.c
+++ bin.c
@@ -6,8 +6,7 @@
  * @cmd: ptr tocmd cmd_prop_t
  * Return: int
  */
-int Bin_exit(__attribute__((__unused__)) shell_data_t *shell_d,
-	cmd_prop_t *cmd)
+int Bin_exit(shell_data_t *shell_d, cmd_prop_t *cmd)
 {
 	int ex_code = 0;
 
